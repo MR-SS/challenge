94debfd706c67a960f352cad7e3d016954bf6a16
MR-SS <sajjadrahimnouri@gmail.com>
2022-08-20 05:19:22 +0430

    submit/generate coupon api

--- router/user.py
+++ router/user.py
@@ -2,11 +2,14 @@
 from fastapi import Body, Depends, Path, Query, Response, APIRouter, Header, Request
 from pydantic import BaseModel
 from typing import Optional, List, Dict
-from schemas import UserBase, UserDisplay, CouponCount
+from schemas import UserBase, UserDisplay, CouponCount, Valid_coupon
 from database import db_user
 from database.db import get_db
 from database.db_user import get_all_users, make_gift
 from auth.oauth2 import reusable_oath, decode_token
+from database.models import User_transaction
+from fastapi.exceptions import HTTPException
+
 
 
 router = APIRouter(tags=['user'])
@@ -34,9 +37,41 @@ def get_all_users(db=Depends(get_db)):  # ,token:str=Depends(oauth2_scheme)):
 @router.post("/generate-coupon", tags=["coupon"])
 def generate_coupon(request: Request, count: CouponCount, db=Depends(get_db)):
     token = request.headers.get("Authrization")
+    if token == None :
+        raise HTTPException(401,"no authorize header")
     num = count.count
     user_auth = decode_token(token)
     if (user_auth["is_admin"] == True):
         return make_gift(num, db)
     else:
-        return 'raise 403'
+        raise HTTPException(401,"Not Authorize user!!")
+        
+@router.post("/submit-coupon", tags=["coupon"])
+def submit_coupon(request:Request,coupon: Valid_coupon ,db=Depends(get_db)):
+    code = coupon.code
+    '''
+        can check only users can add coupon
+    '''
+    token = request.headers.get("Authrization")
+    if token == None :
+        raise HTTPException(401,"no authorize header")
+    user_auth = decode_token(token)
+    user_object = db.query(User_transaction).filter_by(user_id=user_auth["username"]).first()
+    if( user_object == None):
+        # print("mitooni")
+        def add_transaction(username,code:int, db=Depends(get_db)):
+            coupon_winner = db.query(User_transaction).filter_by(user=user_auth["username"]).first()
+            db.add(User_transaction(
+                user = coupon_winner,
+                code = code
+            ))
+            db.commit()
+            return  db.query(User_transaction).all()
+        return add_transaction(user_auth["username"],code,db)
+        '''
+            RACE CONDITION
+        '''
+
+    else:
+        print ("nmitooni")
+
