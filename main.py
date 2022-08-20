1c16dcfa2b734c06a5678cc2800c0d8270d40012
MR-SS <sajjadrahimnouri@gmail.com>
2022-08-20 05:18:37 +0430

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from typing import Union
+from typing import List
+from router import user
+from fastapi import Depends, FastAPI
+from database import models
+from database.models import Dbuser
+from database.db import engine, get_db
+from sqlalchemy.orm import Session
+from database.hash import Hash
 
-from fastapi import FastAPI
 
+from database.db import init_database
+
+
+init_database()
 app = FastAPI()
+app.include_router(user.router)
+# app.include_router(authentication.router)
+session = Session(engine)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"hello": "world"}
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
